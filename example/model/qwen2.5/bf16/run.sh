3555c7958c3e8ff3a76d7ae2fc6e29f42fd0a3fa
focusunsink <549303468@qq.com>
2024-11-27 10:28:56 +0800

    Update run.sh

--- example/model/qwen2.5/bf16/run.sh
+++ example/model/qwen2.5/bf16/run.sh
@@ -1,1 +1,11 @@
-
+export SGLANG_FLASHINFER_USE_TENSOR_CORE=true 
+nsys profile \
+-o ./qwen2.5_3b_fp8_sgl.nsys-rep -ftrue \
+-t cuda,nvtx,osrt --delay=5 \
+--cuda-graph-trace=node \
+--cudabacktrace=all \
+python3 -m sglang.bench_offline_throughput \
+--model ./Qwen2.5-3B \
+--enable-torch-compile  \
+--dataset-name random \
+--num-prompt 1 --random-input 6000 --random-output 128 --random-range 1
